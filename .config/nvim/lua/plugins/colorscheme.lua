8a219e0b0c9e98788aef5e71c0435a43b98b73f0
Syn <xbrqckk@gmail.com>
2025-10-19 21:29:05 +0530

    Add files via upload

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,50 @@
+-- Transparent Tokyonight with LazyVim
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,            -- make Normal/side panels bg = none
+      styles = {
+        sidebars = "transparent",    -- Neo-tree, help, qf
+        floats = "c.bg",      -- Telescope, LSP popups
+      },
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight")
+
+      -- Harden transparency across common UI groups
+      local groups = {
+        "Normal",
+        "NormalNC",
+        "NormalFloat",
+        "FloatBorder",
+        "SignColumn",
+        "MsgArea",
+        "Pmenu",
+        "PmenuSel",
+        "PmenuSbar",
+        "StatusLine",
+        "StatusLineNC",
+        "WinSeparator",
+        "LineNr",
+        "FoldColumn",
+        "CursorLineNr",
+        "TelescopeNormal",
+        "TelescopeBorder",
+        "DiagnosticVirtualTextError",
+        "DiagnosticVirtualTextWarn",
+        "DiagnosticVirtualTextInfo",
+        "DiagnosticVirtualTextHint",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+      }
+      for _, g in ipairs(groups) do
+        vim.api.nvim_set_hl(0, g, { bg = "none" })
+      end
+    end,
+  },
+}
+
